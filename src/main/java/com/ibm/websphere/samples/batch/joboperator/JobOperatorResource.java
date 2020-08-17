f5fd58420431e0e1d7dce8450b3fbe9953c15aad
Scott Kurz <skurz@us.ibm.com>
2020-08-16 21:00:17 -0400

    Basic auth on IT submission
    Signed-off-by: Scott Kurz <skurz@us.ibm.com>

--- src/main/java/com/ibm/websphere/samples/batch/joboperator/JobOperatorResource.java
+++ src/main/java/com/ibm/websphere/samples/batch/joboperator/JobOperatorResource.java
@@ -1,13 +1,61 @@
 package com.ibm.websphere.samples.batch.joboperator;
 
+import java.util.Properties;
+
+import javax.batch.operations.JobOperator;
+import javax.batch.runtime.BatchRuntime;
+import javax.batch.runtime.JobExecution;
+import javax.batch.runtime.JobInstance;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 @Path("/resource")
 public class JobOperatorResource {
 
-    @GET
+    /**
+     * The batch JobOperator
+     */
+    private JobOperator jobOperator;
+
+	@GET
     public String getRequest() {
         return "Batch is in the house";
     }
+    
+	@GET
+    @Path("{jsl}")
+    public long start(@PathParam("jsl") String jobXMLName) {
+
+        Properties jobParameters = getJobParameters("jobParameters");
+
+        JobOperator jobOperator = getJobOperator();
+        long execId = jobOperator.start(jobXMLName, jobParameters);
+        
+        return execId;
+
+        /*
+        JobInstance jobInstance = jobOperator.getJobInstance(execId);
+        JobExecution jobExecution = jobOperator.getJobExecution(execId);
+
+        getResponseWriter().setHttpServletResponse( response )
+                           .beginResponse(HttpServletResponse.SC_OK)
+                           .println( "start(jobXMLName=" + jobXMLName + ", jobParameters=" + jobParameters + "): Job started!" )
+                           .printJobInstance( jobInstance )
+                           .printJobExecution( jobExecution )
+                           .endResponse();
+                           */
+    }
+
+	private Properties getJobParameters(String string) {
+		return new Properties();
+	}
+    /**
+     * @return the batch JobOperator
+     */
+    protected JobOperator getJobOperator() {
+        return (jobOperator != null) ? jobOperator : (jobOperator = BatchRuntime.getJobOperator());
+    }
+
 }
--- src/test/java/com/ibm/websphere/samples/batch/test/BPRSIT.java
+++ src/test/java/com/ibm/websphere/samples/batch/test/BPRSIT.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.microshed.testing.SharedContainerConfig;
+import org.microshed.testing.jaxrs.BasicAuthConfig;
 import org.microshed.testing.jaxrs.RESTClient;
 import org.microshed.testing.jupiter.MicroShedTest;
 
@@ -15,12 +16,18 @@ import com.ibm.websphere.samples.batch.joboperator.JobOperatorResource;
 public class BPRSIT {
 
 	@RESTClient
+	@BasicAuthConfig(user = "bob", password="bobpwd")
 	public static JobOperatorResource appService;
 
 
 	@Test
 	public void testAppResponse() {
 		assertEquals("Batch is in the house", appService.getRequest());
+	}
+	
+	@Test
+	public void testStartSimpl() {
+		appService.start("SimpleBonusPayoutJob");
 	}
 
 }
